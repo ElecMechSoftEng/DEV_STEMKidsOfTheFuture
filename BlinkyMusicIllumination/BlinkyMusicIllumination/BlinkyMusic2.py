6c02314846b9ce12bf282e20a6289247de18bb54
ElecMechSoftEng <42491507+ElecMechSoftEng@users.noreply.github.com>
2019-12-08 20:21:37 -0500

    modified pin_map and setup to test corilation between the pin and pin_mapping. I want it to just operate.

--- BlinkyMusicIllumination/BlinkyMusicIllumination/BlinkyMusic2.py
+++ BlinkyMusicIllumination/BlinkyMusicIllumination/BlinkyMusic2.py
@@ -31,7 +31,10 @@ blink_B2      = int(0)
 logical_map = [0 for i in range(9)]
 
 # Defines the mapping of the GPIO1-8 to the pin on the Pi
-pin_map = [0,11,12,8,15,16,18,22,7]
+# pin_map = [0,11,12,8,15,16,18,22,7]
+#         [0,1,2,3,4,05,06,07,08,9,10,11,12,13,14,15,16,17,18,19,20] 
+pin_map = [0,0,3,5,7,29,31,26,24]
+
 
 # Defines an arbitrary X,Y position for each LED in the star
 # which is used for some star effects
