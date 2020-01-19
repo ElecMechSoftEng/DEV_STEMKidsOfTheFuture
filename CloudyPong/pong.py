a2c10e1039298543de8760c1d101019de966176b
ElecMechSoftEng <42491507+ElecMechSoftEng@users.noreply.github.com>
2020-01-19 13:55:36 -0500

    Added Pong Program to Respository
    Initial Window creation

---
+++ CloudyPong/pong.py
@@ -0,0 +1,11 @@
+# Python Pong Game
+
+import turtle
+
+# Windows
+wn = turtle.Screen()
+wn.title("Pong By CloudSpace")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)     # Size of screen in pixel dimentions
+wn.tracer(0)                        # Helps with refresh rate of window
+
