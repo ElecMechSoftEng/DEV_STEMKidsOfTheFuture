43c8e0af62ce1b9d4e84e51e8300358a37f6f407
ElecMechSoftEng <42491507+ElecMechSoftEng@users.noreply.github.com>
2020-01-19 14:23:54 -0500

    Added Both paddles (Left and Right)

--- CloudyPong/pong.py
+++ CloudyPong/pong.py
@@ -9,6 +9,29 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)     # Size of screen in pixel dimentions
 wn.tracer(0)                        # Helps with refresh rate of window
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)  # Max Speed of Animation
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0) # Initial position
+
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)  # Max Speed of Animation
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0) # Initial position
+
+
+# Ball
+
+
 # Main Game Loop
 while True:
     wn.update()
