b1e27220951658a908cdfd9089f4d4f97bd850da
ElecMechSoftEng <42491507+ElecMechSoftEng@users.noreply.github.com>
2020-01-19 14:14:23 -0500

    Window Open Success

--- CloudyPong/pong.py
+++ CloudyPong/pong.py
@@ -9,3 +9,6 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)     # Size of screen in pixel dimentions
 wn.tracer(0)                        # Helps with refresh rate of window
 
+# Main Game Loop
+while True:
+    wn.update()
